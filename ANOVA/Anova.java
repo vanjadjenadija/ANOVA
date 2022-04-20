04a319c3a8ad5e1ef643a80e59843095eace4535
Vanja Đenadija <vanja.djenadija@student.etf.unibl.org>
2022-04-20 21:18:25 +0200

    Initial commit

---
+++ ANOVA/Anova.java
@@ -0,0 +1,198 @@
+
+import org.apache.commons.math3.distribution.FDistribution;
+import org.apache.commons.math3.distribution.TDistribution;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Anova {
+    private double[][] values;
+    private double[] meanAlternatives;
+    private double[][] errors;
+    private double[] alphas;
+    private double totalMeanAlternatives;
+    private double SST;
+    private double SSE;
+    private double SSA;
+    private double meanSquareAlternatives;
+    private double meanSquareErrors;
+    private double computedF;
+    private double tableF;
+    public List<String> contrastResults = new ArrayList<>();
+    //private double c; // za računanje kontrasta
+    private int n; // broj mjerenja
+    private int k; // broj alternativa
+
+    public Anova(int n, int k, double[][] values) {
+        this.n = n;
+        this.k = k;
+        this.values = values;
+        meanAlternatives = new double[k];
+        errors = new double[n][k];
+        alphas = new double[k];
+        calculateMeanOfAlternatives();
+        calculateErrors();
+        calculateTotalMeanAlternatives();
+        calculateAlphas();
+        calculateSSE();
+        calculateSSA();
+        calculateSST();
+        calculateMeanSquareAlternatives();
+        calculateMeanSquareErrors();
+        calculateComputedF();
+        calculateTableF();
+        calculateCAll();
+    }
+
+    public void calculateCAll() {
+        for (int i = 0; i < k - 1; i++) {
+            for (int j = i + 1; j < k; j++) {
+                double c = calculateC(i, j);
+                double sc = calculateSC();
+                TDistribution tDistribution = new TDistribution(getDegFreedomErrors(), 0.95);
+                double x = 0.0, step = 0.0001;
+                while (Math.abs(tDistribution.cumulativeProbability(x) - 0.95) > step)
+                    x += step;
+                double c1 = c - sc * x;
+                double c2 = c + sc * x;
+                if (c1 < 0.0 && c2 > 0.0)
+                    contrastResults.add(String.format("Alternative %d i %d se ne razlikuju.", i, j));
+                else
+                    contrastResults.add(String.format("Alternative %d i %d se razlikuju.", i, j));
+            }
+        }
+    }
+
+    public double calculateSC() {
+        double se = getMeanSquareErrors();
+        return Math.sqrt(se * 2.0 / (k * n));
+    }
+
+    public double calculateC(int a1, int a2) {
+        return alphas[a1] - alphas[a2];
+    }
+
+    public void calculateTableF() {
+        // za interval povjerenja 95%
+        FDistribution fDistribution = new FDistribution(getDegFreedomAlternatives(), getDegFreedomErrors(), 0.95);
+        double x = 0.0, step = 0.000001;
+        while (Math.abs(fDistribution.cumulativeProbability(x) - 0.95) > step)
+            x += step;
+        tableF = x;
+    }
+
+    public void calculateComputedF() {
+        computedF = meanSquareAlternatives / meanSquareErrors;
+    }
+
+    public void calculateMeanSquareErrors() {
+        meanSquareErrors = SSE / getDegFreedomErrors();
+    }
+
+    public void calculateMeanSquareAlternatives() {
+        meanSquareAlternatives = SSA / getDegFreedomAlternatives();
+    }
+
+    public void calculateSST() {
+        SST = SSA + SSE;
+    }
+
+    public void calculateSSA() {
+        double sum = 0.0;
+        for (int i = 0; i < k; i++) {
+            sum += (alphas[i] * alphas[i]);
+        }
+        SSA = n * sum;
+    }
+
+    public void calculateSSE() {
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                sum += (errors[i][j] * errors[i][j]);
+            }
+        }
+        SSE = sum;
+    }
+
+    public void calculateAlphas() {
+        for (int i = 0; i < k; i++) {
+            alphas[i] = meanAlternatives[i] - totalMeanAlternatives;
+        }
+    }
+
+    public void calculateTotalMeanAlternatives() {
+        double sum = 0.0;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < k; j++) {
+                sum += values[i][j];
+            }
+        totalMeanAlternatives = sum / (k * n);
+    }
+
+    public void calculateErrors() {
+        //System.out.print("\n Errors");
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < k; j++) {
+                errors[i][j] = values[i][j] - meanAlternatives[j];
+                //System.out.print(errors[i][j] + " ");
+            }
+    }
+
+    private void calculateMeanOfAlternatives() {
+        //System.out.print("Srednja vrijednost alternativa ");
+        for (int i = 0; i < k; i++) {
+            meanAlternatives[i] = getMeanOfAlternative(i);
+        }
+    }
+
+    private double getMeanOfAlternative(int j) {
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += values[i][j];
+        }
+        //System.out.print((sum / n) + " ");
+        return sum / n;
+    }
+
+    public int getDegFreedomAlternatives() {
+        return k - 1;
+    }
+
+    public int getDegFreedomErrors() {
+        return k * (n - 1);
+    }
+
+    public int getDegFreedomTotal() {
+        return getDegFreedomAlternatives() + getDegFreedomErrors();
+    }
+
+    public double getSSE() {
+        return SSE;
+    }
+
+    public double getSSA() {
+        return SSA;
+    }
+
+    public double getSST() {
+        return SST;
+    }
+
+    public double getMeanSquareAlternatives() {
+        return meanSquareAlternatives;
+    }
+
+    public double getMeanSquareErrors() {
+        return meanSquareErrors;
+    }
+
+    public double getComputedF() {
+        return computedF;
+    }
+
+    public double getTableF() {
+        return tableF;
+    }
+}
---
+++ ANOVA/Main.java
@@ -0,0 +1,179 @@
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.text.DecimalFormat;
+import java.util.regex.Pattern;
+
+
+public class Main extends Application {
+
+    private static final DecimalFormat df4 = new DecimalFormat("0.0000");
+    private static final DecimalFormat df2 = new DecimalFormat("0.00");
+    private final Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+    private static int ROW;
+    private static int COL;
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        BorderPane root = new BorderPane();
+        ScrollPane scrollPane = new ScrollPane(); // center
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+        GridPane grid = new GridPane();
+        scrollPane.setContent(grid);
+        VBox vBox = new VBox();
+
+        // elementi HBox
+        TextField tfBrMjerenja = new TextField();
+        tfBrMjerenja.setPromptText("Broj mjerenja");
+        tfBrMjerenja.setFocusTraversable(false);
+        TextField tfBrAlternativa = new TextField();
+        tfBrAlternativa.setPromptText("Broj alternativa");
+        tfBrAlternativa.setFocusTraversable(false);
+
+        Button buttonOK = new Button("OK");
+        buttonOK.setOnAction(e -> {
+            Integer brMjerenja = Integer.valueOf(tfBrMjerenja.getText());
+            ROW = brMjerenja;
+            Integer brAlternativa = Integer.valueOf(tfBrAlternativa.getText());
+            COL = brAlternativa;
+            grid.getChildren().clear();
+            vBox.getChildren().clear();
+            drawMatrix(brMjerenja, brAlternativa, grid); // iscrtavanje matrice TextField-a
+        });
+
+
+        Button buttonCalculate = new Button("Izračunaj");
+        buttonCalculate.setOnAction(e -> {
+            vBox.getChildren().clear(); // očistimo prethodni rezultat
+
+            if (ROW == 0 || COL == 0) // ako nismo postavili vrijednosti n i k
+                return;
+
+            if (grid.getChildren().stream().anyMatch(node -> ((TextField) node).getText().equals("")) ||
+                    grid.getChildren().stream().anyMatch(node -> !pattern.matcher(((TextField) node).getText()).matches())) // ako neko od polja nije uneseno
+                return;
+
+            // izračunavamo tek kad smo popunili matricu
+            // učitavanje unesenih vrijednosti iz GUI u matricu
+            double[][] matrix = new double[ROW][COL];
+            Node result = null;
+            ObservableList<Node> childrens = grid.getChildren();
+            for (Node node : childrens) {
+                int row = GridPane.getRowIndex(node);
+                int col = GridPane.getColumnIndex(node);
+                matrix[row][col] = Double.parseDouble(((TextField) node).getText());
+            }
+
+            Anova a = new Anova(ROW, COL, matrix);
+
+            Label lblSSA = new Label("SSA: " + df4.format(a.getSSA()));
+            Label lblSSE = new Label("SSE: " + df4.format(a.getSSE()));
+            Label lblSST = new Label("SST: " + df4.format(a.getSST()));
+            Label lblDegA = new Label("Stepen slobode [A]: " + a.getDegFreedomAlternatives());
+            Label lblDegE = new Label("Stepen slobode [E]: " + a.getDegFreedomErrors());
+            Label lblDegT = new Label("Stepen slobode [T]: " + a.getDegFreedomTotal());
+            Label lblSqrA = new Label("Varijansa sume kvadrata [A]: " + df4.format(a.getMeanSquareAlternatives()));
+            Label lblSqrE = new Label("Varijansa sume kvadrata [E]: " + df4.format(a.getMeanSquareErrors()));
+            Label lblF = new Label("F izračunato: " + df2.format(a.getComputedF()));
+            Label lblFtable = new Label("F tabelarno: " + df2.format(a.getTableF()));
+            Label lblResult;
+            if (a.getComputedF() > a.getTableF())
+                lblResult = new Label("Razlike između alternativa su statistički značajne.");
+            else
+                lblResult = new Label("Razlike između alternativa nisu statistički značajne.");
+
+            vBox.getChildren().addAll(lblSSA, lblSSE, lblSST, lblDegA, lblDegE, lblDegT, lblSqrA, lblSqrE, lblF, lblFtable, lblResult);
+
+            for (String conResult : a.contrastResults) {
+                vBox.getChildren().add(new Label(conResult));
+            }
+            vBox.setAlignment(Pos.TOP_LEFT);
+            vBox.setSpacing(10);
+            vBox.setPrefHeight(Region.USE_PREF_SIZE);
+            vBox.setMaxWidth(Region.USE_PREF_SIZE);
+            root.setRight(vBox);
+        });
+
+        HBox hbox = new HBox(tfBrMjerenja, tfBrAlternativa, buttonOK);
+        HBox.setMargin(tfBrMjerenja, new
+
+                Insets(10, 10, 10, 10));
+        HBox.setMargin(tfBrAlternativa, new
+
+                Insets(10, 10, 10, 10));
+        hbox.setAlignment(Pos.CENTER);
+
+        root.setTop(hbox);
+        root.setCenter(scrollPane);
+        root.setBottom(buttonCalculate);
+
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(2);
+        grid.setVgap(2);
+
+        BorderPane.setAlignment(hbox, Pos.CENTER);
+        BorderPane.setAlignment(scrollPane, Pos.CENTER);
+        BorderPane.setAlignment(buttonCalculate, Pos.CENTER);
+        BorderPane.setAlignment(vBox, Pos.CENTER_LEFT);
+        BorderPane.setMargin(grid, new
+
+                Insets(10, 10, 10, 10));
+        BorderPane.setMargin(buttonCalculate, new
+
+                Insets(10, 10, 10, 10));
+        BorderPane.setMargin(vBox, new
+
+                Insets(0, 10, 10, 10));
+        Scene scene = new Scene(root, 1200, 600);
+        primaryStage.setTitle("ANOVA & contrasts");
+        String path = ".\\ANOVA\\aaa.png";
+        Image applicationIcon = new Image(new File(path).toURI().toString());
+        primaryStage.getIcons().add(applicationIcon);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void drawMatrix(Integer brMjerenja, Integer brAlternativa, GridPane grid) {
+        for (int x = 0; x < brMjerenja; x++) {
+            for (int y = 0; y < brAlternativa; y++) {
+
+                // Create a new TextField in each Iteration
+                TextField tf = new TextField();
+                tf.setMaxWidth(70);
+                tf.setMaxWidth(20);
+                tf.setMinWidth(70);
+                tf.setMinHeight(20);
+
+                tf.setAlignment(Pos.CENTER);
+                tf.setPromptText(String.format("[%d,%d]", x, y));
+                // Iterate the Index using the loops
+                GridPane.setRowIndex(tf, x);
+                GridPane.setColumnIndex(tf, y);
+                grid.getChildren().add(tf);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+
+            /*double[][] zlaja = {{0.0972, 0.1382, 0.7966}
+                    , {0.0971, 0.1432, 0.5300}
+                    , {0.0969, 0.1382, 0.5152}
+                    , {0.1954, 0.1730, 0.6675},
+                    {0.0974, 0.1383, 0.5298}};*/
